e1dde0be8bb0197d1d43b69fabd46bef72238668
sweeppy <104524272+sweeppy@users.noreply.github.com>
2026-04-12 20:08:55 +0300

    [New] Add swagger extension and register it in Program.cs

---
+++ backend/Chess/Chess.API/Extensions/SwaggerExtension.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.OpenApi;
+
+namespace Chess.API.Extensions;
+
+public static class SwaggerExtension
+{
+    public static IServiceCollection AddCustomSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Chess API",
+                Version = "1.0"
+            });
+
+            options.AddSecurityDefinition("bearer", new OpenApiSecurityScheme
+            {
+                Type = SecuritySchemeType.Http,
+                Scheme = "bearer",
+                BearerFormat = "JWT",
+                Description = "JWT Authorization header using the Bearer scheme."
+            });
+
+            options.AddSecurityRequirement(document => new OpenApiSecurityRequirement
+            {
+                [new OpenApiSecuritySchemeReference("bearer", document)] = []
+            });
+        });
+
+        return services;
+    }
+
+    public static IApplicationBuilder UseCustomSwagger(this IApplicationBuilder app, IHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Account API v1.0");
+                options.RoutePrefix = string.Empty;
+            });
+        }
+
+        return app;
+    }
+
+}
