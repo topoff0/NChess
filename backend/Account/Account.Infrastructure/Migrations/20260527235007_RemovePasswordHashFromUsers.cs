b6d8b6c493db4ceae97f6c1b9c5dccc98a83fb28
topoff0 <vikonotopoff@gmail.com>
2026-05-28 02:53:56 +0300

    refactor(account): remove password hash from users

---
+++ backend/Account/Account.Infrastructure/Migrations/20260527235007_RemovePasswordHashFromUsers.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Account.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovePasswordHashFromUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                schema: "account",
+                table: "users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordHash",
+                schema: "account",
+                table: "users",
+                type: "character varying(512)",
+                maxLength: 512,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
